55a0abb35541df08f9810b583ae631f730662636
Mário Prada <mariosilvaprada@gmail.com>
2019-09-30 10:47:08 +0100

    updating

--- src/views/Cats.vue
+++ src/views/Cats.vue
@@ -1,17 +1,26 @@
 <template>
   <div>
-    <h1>Cats for Adoption</h1>
-    <b-table striped hover :items="cats"></b-table>
+    <PetTable
+      species="cats"
+      :pets="cats"
+    />
   </div>
 </template>
 
 <script>
-import cats from '@/data/cats'
-  export default {
-    data() {
-      return {
-        cats
-      }
-    }
+import { mapState } from 'vuex'
+import PetTable from '@/components/PetTable.vue'
+export default {
+  components: {
+    PetTable
+  },
+  data() {
+    return {}
+  },
+  computed: {
+    ...mapState([
+      'cats'
+    ])
   }
+}
 </script>
